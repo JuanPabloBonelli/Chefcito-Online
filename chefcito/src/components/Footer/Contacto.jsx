7f013d60b669f398a6e3161c732153920ded8c34
Juan Pablo Bonelli <juampibonelli@hotmail.com>
2025-01-02 19:43:04 -0300

    Ajustes

--- chefcito/src/components/Footer/Contacto.jsx
+++ chefcito/src/components/Footer/Contacto.jsx
@@ -1,14 +1,28 @@
-import React from 'react'
-import "./Contacto.css"
+import React from 'react';
+import "./Contacto.css";
 
 const Contacto = () => {
   return (
     <>
-    <section>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero blanditiis minus sapiente, hic adipisci odit velit commodi, eligendi aspernatur asperiores vel! Necessitatibus iste accusamus esse cum itaque temporibus, earum libero.</p>
-      </section>              
-    </>
-  )
-}
+    <section className="contacto">
+      <h2>Contacto</h2>
+      <p>Si tienes alguna pregunta, sugerencia o comentario, ¡nos encantaría saber de ti!</p>
+      <form className="contacto-form">
+        <label htmlFor="nombre">Nombre</label>
+        <input type="text" id="nombre" name="nombre" placeholder="Tu nombre" required />
 
-export default Contacto
+        <label htmlFor="email">Correo</label>
+        <input type="email" id="email" name="email" placeholder="tuemail@ejemplo.com" required />
+
+        <label htmlFor="mensaje">Mensaje</label>
+        <textarea id="mensaje" name="mensaje" rows="5" placeholder="Escribe tu mensaje aquí" required></textarea>
+
+        <button type="submit" className="btn-enviar">Enviar</button>
+      </form>
+    </section>
+    </>
+  );
+};
+
+export default Contacto;
+
